cf60b54bd516b203318f464c95564a296b068eb4
noakash <noakash@ymail.com>
2025-03-28 13:55:48 +0530

    typescript fix

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,10 @@
 
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
+  typescript: {
+    typeCheck: false
+
+  },
   devtools: { enabled: true },
   css: ['~/assets/css/main.css'],
   modules: ['@nuxtjs/tailwindcss', 'shadcn-nuxt', '@nuxt/icon'],
